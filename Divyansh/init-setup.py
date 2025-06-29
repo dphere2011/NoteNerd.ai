2a0d5640b480d822270100be733c3d467273aed4
TehPopsicle <parthivmenon2011@outlook.com>
2025-06-30 00:47:12 +0530

    Updated init-setup.py and run.py

--- Divyansh/init-setup.py
+++ Divyansh/init-setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import subprocess
+import os
+from pathlib import Path
 
 def install_requirements():
     subprocess.check_call(["pip", "install", "--upgrade", "pip"])
@@ -9,6 +11,9 @@ def install_requirements():
 def first_run():
     subprocess.run(["python", "run.py"])
     
+def create_flag():
+    Path("setup_done.flag").write_text("Setup complete")
 
 install_requirements()
 first_run()
+create_flag()
